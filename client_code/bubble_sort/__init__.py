37e03dc2d1651e7ddede8cfee4d79968788b7497
vobaocanh.ltp22@gmail.com <vobaocanh.ltp22@gmail.com>
2024-08-23 03:26:52 +0000

    Edited bubble_sort

--- client_code/bubble_sort/__init__.py
+++ client_code/bubble_sort/__init__.py
@@ -1,18 +1,14 @@
 import anvil.server
 from ._anvil_designer import bubble_sortTemplate
 def bubble_sort(arr):  
-  # Outer loop to iterate jthrough the list n times
   for n in range(len(arr) - 1, 0, -1):    
     swapped = False    
-    # Inner loop to compare adjacent elements
     for i in range(n):
       if arr[i] > arr[i + 1]:
-        # Swap elements if they are in the wrong order
         swapped = True
         arr[i], arr[i + 1] = arr[i + 1], arr[i]      
       if not swapped:
         return
-# Sample list to be sorted
 arr = [39, 12, 18, 85, 72, 10, 2, 18]
 print("Unsorted list is:")
 print(arr)
