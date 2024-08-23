b01e2373057aaf8948d3377c4012180323784620
vobaocanh.ltp22@gmail.com <vobaocanh.ltp22@gmail.com>
2024-08-23 02:03:29 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,10 +1,19 @@
-from ._anvil_designer import Form1Template
-from anvil import *
-
-
-class Form1(Form1Template):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
+def bubble_sort(arr):  
+  # Outer loop to iterate through the list n times
+  for n in range(len(arr) - 1, 0, -1):    
+    swapped = False    
+    # Inner loop to compare adjacent elements
+    for i in range(n):
+      if arr[i] > arr[i + 1]:
+        # Swap elements if they are in the wrong order
+        swapped = True
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]      
+      if not swapped:
+        return
+# Sample list to be sorted
+arr = [39, 12, 18, 85, 72, 10, 2, 18]
+print("Unsorted list is:")
+print(arr)
+bubble_sort(arr)
+print("Sorted list is:")
+print(arr)
